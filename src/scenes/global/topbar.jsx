785838a6c1424c00dfcb6282ee64506d5d1290d9
Arthur Averbuch <arthurav@cuest.io>
2023-03-14 23:45:13 +0200

    test toolbar

--- src/scenes/global/topbar.jsx
+++ src/scenes/global/topbar.jsx
@@ -1,15 +1,19 @@
+import { Button } from '@aws-amplify/ui-react';
 import PersonIcon from '@mui/icons-material/Person';
 import './topbar.css';
 
 const Topbar = (props) =>{
-    
+    console.log(props);
     return (
         <div className="topBar">
             <div className='topBarTitle'>
-                {props.userProfile.account}
+                {props.userProfile.attributes.given_name}
             </div>
             
             <div className="userProfile">
+                <div className="userIcon">
+                  <button onClick={props.action}>Sign Out</button>
+                </div>
                 <div className="userIcon">
                     <PersonIcon sx={{ fontSize: 36 }} color="action"/>
                 </div>
