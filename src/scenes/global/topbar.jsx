71509d01b669ae4a924d98accb3e24d781620885
Arthur Averbuch <arthurav@cuest.io>
2023-03-16 22:55:52 +0200

    updates in the menu

--- src/scenes/global/topbar.jsx
+++ src/scenes/global/topbar.jsx
@@ -1,23 +1,65 @@
 import PersonIcon from '@mui/icons-material/Person';
 import './topbar.css';
+import { Menu, MenuItem, Grow } from "@material-ui/core";
+import { useState } from "react";
+
 
 const Topbar = (props) =>{
-    
+    console.log(props);
+
+  const [anchor, setAnchor] = useState(null);
+  
+  const [selected, setSelected] = useState(-1);
+
+  const openMenu = (event) => {
+    setAnchor(event.currentTarget);
+  };
+
+  const closeMenu = () => {
+    setAnchor(null);
+  };
+
+  const onMenuItemClick = (event, index) => {
+    setAnchor(null);
+    setSelected(index);
+  };
+
+
     return (
         <div className="topBar">
             <div className='topBarTitle'>
                 {props.userProfile.account}
             </div>
             
-            <div className="userProfile">
+            <div className="userProfile" onClick={openMenu} variant="contained">
                 <div className="userIcon">
                     <PersonIcon sx={{ fontSize: 36 }} color="action"/>
                 </div>
                 <div className='userName'>
-                    <b>{props.userProfile.firstName} {props.userProfile.lastName}</b>
+                    <b>{props.userProfile.attributes.given_name} {props.userProfile.attributes.family_name}</b>
                 </div>
             </div>
-
+      <Menu
+        open={Boolean(anchor)}
+        anchorEl={anchor}
+        onClose={closeMenu}
+        keepMounted
+        TransitionComponent={Grow}
+        PaperProps={{
+          style: {
+            maxHeight: 30 * 4,
+            width: "20ch",
+          },
+        }}
+      >
+          <MenuItem
+            key="1"
+            onClick={props.action}
+            selected={1 === selected}
+          >
+            Sign out
+          </MenuItem>
+      </Menu>
 
         </div>
     );
