9269d9313652a9c9281fb8e43e6c6b6cc2b98be9
Arthur Averbuch <arthurav@cuest.io>
2023-03-23 09:55:01 +0200

    auth screen

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { Amplify, Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import '@aws-amplify/ui-react/styles.css'
 import { Authenticator, CheckboxField, useAuthenticator } from "@aws-amplify/ui-react";
-
+import {components, formFields} from './components/auth';
 Amplify.configure(awsconfig);
 
  
@@ -25,73 +25,8 @@ function App() {
     params.set(key, value);
   });
  
-  const formFields = {
-    signIn: {
-      username: {
-        placeholder: 'Enter your email',
-      }
-    },
-    signUp: {
-      email: {
-        label: "Username",
-        placeholder: 'Enter your email',
-        order: 1
-      },
-      given_name: {
-        label: "First Name",
-        placeholder: 'Enter you first name',
-        order: 4
-      },
-      family_name: {
-        label: "Last Name",
-        placeholder: 'Enter you last name',
-        order: 5
-      },
-      "custom:Company": {
-        label: "Company",
-        placeholder: 'Enter your company name',
-        order: 6
-      },
-
-      password: {
-        label: 'Password:',
-        placeholder: 'Enter your Password:',
-        order: 2,
-      },
-      confirm_password: {
-        label: 'Confirm Password:',
-        order: 3,
-      },
-    },
-    forceNewPassword: {
-      password: {
-        placeholder: 'Enter your Password:',
-      },
-    },
-    resetPassword: {
-      username: {
-        placeholder: 'Enter your email:',
-      },
-    },
-    confirmResetPassword: {
-      confirmation_code: {
-        placeholder: 'Enter your Confirmation Code:',
-        label: 'New Label',
-        isRequired: false,
-      },
-      confirm_password: {
-        placeholder: 'Enter your Password Please:',
-      },
-    },
-    confirmSignIn: {
-      confirmation_code: {
-        label: 'New Label',
-        placeholder: 'Enter your Confirmation Code:',
-        isRequired: false,
-      },
-    },
-  };
-
+  
+   
   
   return (
       <div className="app">
@@ -99,29 +34,7 @@ function App() {
       <Authenticator
         initialState="signIn"
         formFields={formFields}
-        components={{
-            SignUp: {
-              FormFields() {
-                  const { validationErrors } = useAuthenticator();
-  
-                  return (
-                    <>
-                      {/* Re-use default `Authenticator.SignUp.FormFields` */}
-                      <Authenticator.SignUp.FormFields />
-      
-                      {/* Append & require Terms & Conditions field to sign up  */}
-                      <CheckboxField
-                        errorMessage={validationErrors.acknowledgement}
-                        hasError={!!validationErrors.acknowledgement}
-                        name="acknowledgement"
-                        value="yes"
-                        label="I agree with the Terms & Conditions"
-                      />
-                    </>
-              );
-            },
-          },
-        }}
+        components={components}
         services={{
           async validateCustomSignUp(formData) {
             if (!formData.acknowledgement) {
@@ -132,8 +45,8 @@ function App() {
           },
         }}
       >
-        {({ signOut, user }) => (
-           <div className="app">
+      {({ signOut, user }) => (
+          <div className="app">
            <main className ="content">
            {
                <Topbar userProfile={user} action={signOut}/> 
---
+++ src/components/auth.jsx
@@ -0,0 +1,198 @@
+import { Authenticator, CheckboxField, useAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify, Auth } from 'aws-amplify';
+import { useTheme, View, Image, Text, Heading } from "@aws-amplify/ui-react";
+
+export const components = {
+    Header() {
+      const { tokens } = useTheme();
+  
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Image
+            alt="Cuest logo"
+            src={'/icons/horizontal.png'}
+          />
+        </View>
+      );
+    },
+  
+    Footer() {
+      const { tokens } = useTheme();
+  
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Text color={tokens.colors.neutral[80]}>
+            &copy; All Rights Reserved
+          </Text>
+        </View>
+      );
+    },
+  
+    SignIn: {
+      Header() {
+        const { tokens } = useTheme();
+  
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Sign in to your account
+          </Heading>
+        );
+      },
+    //   Footer() {
+    //     const { toResetPassword } = useAuthenticator();
+  
+    //     return (
+    //       <View textAlign="center">
+    //         <Button
+    //           fontWeight="normal"
+    //           onClick={toResetPassword}
+    //           size="small"
+    //           variation="link"
+    //         >
+    //           Reset Password
+    //         </Button>
+    //       </View>
+    //     );
+    //   },
+    // },
+    },
+    SignUp: {
+        Header() {
+            const { tokens } = useTheme();
+    
+            return (
+            <Heading
+                padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+                level={3}
+            >
+                Create a new account
+            </Heading>
+            );
+        },
+        FormFields() {
+            const { validationErrors } = useAuthenticator();
+
+            return (
+            <>
+                {/* Re-use default `Authenticator.SignUp.FormFields` */}
+                <Authenticator.SignUp.FormFields />
+
+                {/* Append & require Terms & Conditions field to sign up  */}
+                <CheckboxField
+                errorMessage={validationErrors.acknowledgement}
+                hasError={!!validationErrors.acknowledgement}
+                name="acknowledgement"
+                value="yes"
+                label="I agree with the Terms & Conditions"
+                />
+            </>
+            )  ;
+        },
+    },
+    //   Footer() {
+    //     const { toSignIn } = useAuthenticator();
+  
+    //     return (
+    //       <View textAlign="center">
+    //         <Button
+    //           fontWeight="normal"
+    //           onClick={toSignIn}
+    //           size="small"
+    //           variation="link"
+    //         >
+    //           Back to Sign In
+    //         </Button>
+    //       </View>
+    //     );
+    //   },
+    
+    // ConfirmSignUp: {
+    //   Header() {
+    //     const { tokens } = useTheme();
+    //     return (
+    //       <Heading
+    //         padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+    //         level={3}
+    //       >
+    //         Enter Information:
+    //       </Heading>
+    //     );
+    //   },
+    //   Footer() {
+    //     return <Text>Footer Information</Text>;
+    //   },
+    // },
+    
+    
+    
+  };
+
+  export const formFields = {
+    signIn: {
+      username: {
+        placeholder: 'Enter your email',
+      }
+    },
+    signUp: {
+      email: {
+        label: "Username",
+        placeholder: 'Enter your email',
+        order: 1
+      },
+      given_name: {
+        label: "First Name",
+        placeholder: 'Enter you first name',
+        order: 4
+      },
+      family_name: {
+        label: "Last Name",
+        placeholder: 'Enter you last name',
+        order: 5
+      },
+      "custom:Company": {
+        label: "Company",
+        placeholder: 'Enter your company name',
+        order: 6
+      },
+
+      password: {
+        label: 'Password:',
+        placeholder: 'Enter your Password:',
+        order: 2,
+      },
+      confirm_password: {
+        label: 'Confirm Password:',
+        order: 3,
+      },
+    },
+    forceNewPassword: {
+      password: {
+        placeholder: 'Enter your Password:',
+      },
+    },
+    resetPassword: {
+      username: {
+        placeholder: 'Enter your email:',
+      },
+    },
+    confirmResetPassword: {
+      confirmation_code: {
+        placeholder: 'Enter your Confirmation Code:',
+        label: 'New Label',
+        isRequired: false,
+      },
+      confirm_password: {
+        placeholder: 'Enter your Password Please:',
+      },
+    },
+    confirmSignIn: {
+      confirmation_code: {
+        label: 'New Label',
+        placeholder: 'Enter your Confirmation Code:',
+        isRequired: false,
+      },
+    },
+  };
