f0ef922146e76de885eff2c8cb8bf16d13a5d69e
Arthur Averbuch <arthurav@cuest.io>
2023-05-09 22:17:10 +0300

    adding domain env variabes

--- _archive/src/scenes/dashboard/index.jsx
+++ _archive/src/scenes/dashboard/index.jsx
@@ -29,7 +29,7 @@ const Dashboard = () =>{
 
       async function fetchClusters(){
         const response = await axios.get(
-            "".concat(process.env.REACT_APP_REST_URI_DEVICES, "/devices/nodes"),
+            "https://".concat(process.env.REACT_APP_REST_URI,".", process.env.REACT_APP_DOMAIN, "/devices/cluster"),
             {
               headers: {
                 Authorization:  `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`
@@ -41,7 +41,7 @@ const Dashboard = () =>{
     
       async function fetchNodes(){
         const response = await axios.get(
-          "".concat(process.env.REACT_APP_REST_URI_DEVICES, "/devices/agents"),
+          "https://".concat(process.env.REACT_APP_REST_URI,".", process.env.REACT_APP_DOMAIN, "/devices/node"),
           {
             headers: {
                 Authorization:  `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`
