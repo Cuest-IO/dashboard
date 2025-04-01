9f27917c524486539d0cda687ca7c82e16ae0e0e
Daniel Lippo <daniil_sizov22@icloud.com>
2025-04-01 14:11:18 +0300

    fix: add translation to linux instrctions component

--- src/react/ui/pages/AccessKey/LinuxInstructions.tsx
+++ src/react/ui/pages/AccessKey/LinuxInstructions.tsx
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { Auth } from 'aws-amplify';
+import { useTranslation } from 'react-i18next';
 
 import { ClientCredentialsResponse } from '../../../engine/dto/account';
 import { useInstallScript } from '../../../engine/state/installer/useInstallScript';
@@ -15,6 +16,7 @@ type LinuxInstructionsProps = {
 };
 
 const LinuxInstructions = ({ credentials }: LinuxInstructionsProps) => {
+  const { t } = useTranslation();
   const { version, id, secret } = credentials;
 
   const [token, setToken] = useState<string | null>(null);
@@ -55,10 +57,7 @@ const LinuxInstructions = ({ credentials }: LinuxInstructionsProps) => {
   return (
     <Grid container direction="column">
       <Grid item>
-        <Typography variant="body1">
-          Connect your server, VM or a computer to Crowd Cloud by using our installer script, personalized for your
-          account:
-        </Typography>
+        <Typography variant="body1">{t('access_key:install_linux_command')}</Typography>
       </Grid>
       <Grid item xs={12} pt={4}>
         <Grid container alignItems="center">
@@ -74,10 +73,10 @@ const LinuxInstructions = ({ credentials }: LinuxInstructionsProps) => {
                   color: 'rgb(51,51,51)',
                 }}
               >
-                {fullScript || 'Loading command...'}
+                {fullScript || t('access_key:install_linux_command_loading')}
               </Typography>
               <Button onClick={() => setExpanded(prev => !prev)} sx={{ textTransform: 'none' }} size="small">
-                {expanded ? 'Show Less' : 'Show More'}
+                {expanded ? t('access_key:toggle_command_show_less') : t('access_key:toggle_command_show_more')}
               </Button>
             </Box>
           </Grid>
